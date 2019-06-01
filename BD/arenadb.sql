6fadb52d4ea6caebdd41be4b64dca5d428ba5401
Mamun Sourav <souravcuetcse2010@gmail.com>
2019-06-01 04:11:17 -0600

    Database file updated

--- BD/arenadb.sql
+++ BD/arenadb.sql
@@ -11,27 +11,39 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping structure for table arenadb.computer
+CREATE TABLE IF NOT EXISTS `computer` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `brand` varchar(255) DEFAULT NULL,
+  `price` double NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`cid`),
+  KEY `FKa5w2s4lhu7ngssecaictcr06a` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table arenadb.computer: 1 rows
+/*!40000 ALTER TABLE `computer` DISABLE KEYS */;
+INSERT INTO `computer` (`cid`, `brand`, `price`, `user_id`) VALUES
+	(1, 'Dell', 1000.9, 1);
+/*!40000 ALTER TABLE `computer` ENABLE KEYS */;
+
 -- Dumping structure for table arenadb.user
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `FirstName` varchar(100) NOT NULL,
-  `LastName` varchar(100) NOT NULL,
-  `Age` int(11) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `UserName` varchar(100) NOT NULL,
-  `Password` varchar(100) NOT NULL,
-  `Status` int(11) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `userName` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Dumping data for table arenadb.user: ~3 rows (approximately)
+-- Dumping data for table arenadb.user: 1 rows
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `FirstName`, `LastName`, `Age`, `Email`, `UserName`, `Password`, `Status`) VALUES
-	(1, 'dada', 'sourav', 26, 'dada@100.com', 'dada', 'dada123', 1),
-	(2, 'test1', 'TestLName', 29, '112@ddd.com', 'tests', 'pass', 1),
-	(3, 'test1', 'TestLName', 29, '112@ddd.com', 'tests', 'pass', 1),
-	(4, 'test1', 'TestLName', 29, '112@ddd.com', 'tests', 'pass', 1),
-	(5, 'test1', 'TestLName', 29, '112@ddd.com', 'tests', 'pass', 1);
+INSERT INTO `user` (`id`, `age`, `email`, `firstName`, `lastName`, `password`, `status`, `userName`) VALUES
+	(1, 29, '112@ddd.com', 'test1', 'TestLName', 'pass', 1, 'tests');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
